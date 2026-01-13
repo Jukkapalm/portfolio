ac50da07554be590dae72961406e14fc38b721e1
Juginhikipaja <jukka.palm@hotmail.fi>
2026-01-13 20:58:03 +0200

    Testi

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ if (window.innerWidth > 767) {
     );
 
     gsap.from(".banner-txt h1, .banner-txt h3, .banner-txt h6, .banner-txt a", {
-        y: 40,
+        y: 30,
         opacity: 0,
         duration: 0.6,
         stagger: 0.15,
