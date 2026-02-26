42da8707135bfdea4e5fb9f2daae1a93f64643ae
Juginhikipaja <jukka.palm@hotmail.fi>
2026-02-27 01:51:05 +0200

    Poistettu binaarien funktiot

--- js/script.js
+++ js/script.js
@@ -9,28 +9,26 @@ let vaiheViive = 1000;
 let statusViive = 1000;
 
 
-
 // Tekstit tarkistuksiin jotka käynnistys sekvenssissä suoritetaan
-let txt1 = "> INITIALIZING_FUSION_CELL...";
-let txt2 = "> VOLTAGE_STABLE: 1.21GW";
-let txt3 = "> COOLANT_FLOW: OPTIMAL";
-let txt4 = "> HARDWARE_INTEGRITY:";
-let txt5 = "> CONNECTING_CORTEX...";
-let txt6 = "> CPU_CORES: 256_ACTIVE";
-let txt7 = "> SYNAPTIC_LINKS: ESTABLISHED";
-let txt8 = "> NEURAL_LATENCY:";
-let txt9 = "> OPTIC_SENSORS: CALIBRATING...";
-let txt10 = "> THERMAL_VISION: ONLINE";
-let txt11 = "> AUDITORY_BUFFERS: CLEARED";
-let txt12 = "> ENVIRONMENT_MAPPING:";
-let txt13 = "> DETECTING_EXTERNAL_SIGNAL...";
-let txt14 = "> ENCRYPTED_DATA_STREAM_FOUND";
-let txt15 = "> ATTEMPTING_DECRYPTION...";
-let txt16 = "> DECRYPTION_PROGRESS:";
-let txt17 = "> LOADING_PERSONALITY_MATRIX...";
-let txt18 = "> MEMORY_RECALL: ACTIVE";
-let txt19 = "> SYSTEM_STABILITY:"
-
+const txt1 = "> INITIALIZING_FUSION_CELL...";
+const txt2 = "> VOLTAGE_STABLE: 1.21GW";
+const txt3 = "> COOLANT_FLOW: OPTIMAL";
+const txt4 = "> HARDWARE_INTEGRITY:";
+const txt5 = "> CONNECTING_CORTEX...";
+const txt6 = "> CPU_CORES: 256_ACTIVE";
+const txt7 = "> SYNAPTIC_LINKS: ESTABLISHED";
+const txt8 = "> NEURAL_LATENCY:";
+const txt9 = "> OPTIC_SENSORS: CALIBRATING...";
+const txt10 = "> THERMAL_VISION: ONLINE";
+const txt11 = "> AUDITORY_BUFFERS: CLEARED";
+const txt12 = "> ENVIRONMENT_MAPPING:";
+const txt13 = "> DETECTING_EXTERNAL_SIGNAL...";
+const txt14 = "> ENCRYPTED_DATA_STREAM_FOUND";
+const txt15 = "> ATTEMPTING_DECRYPTION...";
+const txt16 = "> DECRYPTION_PROGRESS:";
+const txt17 = "> LOADING_PERSONALITY_MATRIX...";
+const txt18 = "> MEMORY_RECALL: ACTIVE";
+const txt19 = "> SYSTEM_STABILITY:"
 
 
 // Haetaan elementti id:n perusteella
@@ -41,10 +39,8 @@ const phase4Ele = document.getElementById("phase-4");
 const phase5Ele = document.getElementById("phase-5");
 
 
-
 // Funktioiden ketju käynnistys sekvenssiin
 function kaynnistysSekvenssi() {
-    if (i === 0) paivitaBin1(1, 1);
     phase1Ele.style.visibility = "visible";
 
     if (i === 0) {
@@ -62,7 +58,6 @@ function kaynnistysSekvenssi() {
     }
 }
 function tulostaTxt2() {
-    if (i === 0) paivitaBin2(1, 2);
     if (i < txt2.length) {
         phase1Ele.innerHTML += txt2.charAt(i);
         i++;
@@ -74,7 +69,6 @@ function tulostaTxt2() {
     }
 }
 function tulostaTxt3() {
-    if (i === 0) paivitaBin1(1, 3);
     if (i < txt3.length) {
         phase1Ele.innerHTML += txt3.charAt(i);
         i++;
@@ -86,7 +80,6 @@ function tulostaTxt3() {
     }
 }
 function tulostaTxt4() {
-    if (i === 0) paivitaBin2(1, 4)
     if (i < txt4.length) {
         phase1Ele.innerHTML += txt4.charAt(i);
         i++;
@@ -108,7 +101,6 @@ function prosenttiLataus() {
             latausProsentti = 0;
             i = 0;
             pohjaTeksti = "";
-            paivitaBin1(1, 5);
             phase1Ele.innerHTML += "<br><b>> POWER_CORE: ONLINE</b>";
             setTimeout(siirryPhase2, vaiheViive);
         }, statusViive);
@@ -119,7 +111,6 @@ function siirryPhase2() {
     phase1Ele.style.display = "none";
     phase2Ele.style.visibility = "visible";
 
-    if (i === 0) paivitaBin2(2, 1);
     if (i === 0) {
         phase2Ele.innerHTML += "<br><br>";
     }
@@ -135,7 +126,6 @@ function siirryPhase2() {
     }
 }
 function tulostaTxt6() {
-    if (i === 0) paivitaBin1(2, 2);
     if (i < txt6.length) {
         phase2Ele.innerHTML += txt6.charAt(i);
         i++;
@@ -147,7 +137,6 @@ function tulostaTxt6() {
     }
 }
 function tulostaTxt7() {
-    if (i === 0) paivitaBin2(2, 3);
     if (i < txt7.length) {
         phase2Ele.innerHTML += txt7.charAt(i);
         i++;
@@ -159,7 +148,6 @@ function tulostaTxt7() {
     }
 }
 function tulostaTxt8() {
-    if (i === 0) paivitaBin1(2, 4);
     if (i < txt8.length) {
         phase2Ele.innerHTML += txt8.charAt(i);
         i++;
@@ -180,7 +168,6 @@ function msLataus() {
             latausProsentti = 0;
             i = 0;
             pohjaTeksti = "";
-            paivitaBin2(2, 5)
             phase2Ele.innerHTML += "<br><b>> NEURAL_LINK: STABLE</b>";
             setTimeout(siirryPhase3, vaiheViive);
         }, statusViive);
@@ -191,7 +178,6 @@ function siirryPhase3() {
     phase2Ele.style.display = "none";
     phase3Ele.style.visibility = "visible";
 
-    if (i === 0) paivitaBin1(3, 1);
     if (i === 0) {
         phase3Ele.innerHTML += "<br><br>";
     }
@@ -207,7 +193,6 @@ function siirryPhase3() {
     }
 }
 function tulostaTxt10() {
-    if (i === 0) paivitaBin2(3, 2);
     if (i < txt10.length) {
         phase3Ele.innerHTML += txt10.charAt(i);
         i++;
@@ -219,7 +204,6 @@ function tulostaTxt10() {
     }
 }
 function tulostaTxt11() {
-    if (i === 0) paivitaBin1(3, 3);
     if (i < txt11.length) {
         phase3Ele.innerHTML += txt11.charAt(i);
         i++;
@@ -231,7 +215,6 @@ function tulostaTxt11() {
     }
 }
 function tulostaTxt12() {
-    if (i === 0) paivitaBin2(3, 4);
     if (i < txt12.length) {
         phase3Ele.innerHTML += txt12.charAt(i);
         i++;
@@ -252,7 +235,6 @@ function prosenttiLataus2() {
             latausProsentti = 0;
             i = 0;
             pohjaTeksti = "";
-            paivitaBin1(3, 5)
             phase3Ele.innerHTML += "<br><b>> SENSORS: ACTIVE</b>";
             setTimeout(siirryPhase4, vaiheViive);
         }, statusViive);
@@ -263,7 +245,6 @@ function siirryPhase4() {
     phase3Ele.style.display = "none";
     phase4Ele.style.visibility = "visible";
 
-    if (i === 0) paivitaBin2(4, 1);
     if (i === 0) {
         phase4Ele.innerHTML += "<br><br>";
     }
@@ -279,7 +260,6 @@ function siirryPhase4() {
     }
 }
 function tulostaTxt14() {
-    if (i === 0) paivitaBin1(4, 2);
     if (i < txt14.length) {
         phase4Ele.innerHTML += txt14.charAt(i);
         i++;
@@ -291,7 +271,6 @@ function tulostaTxt14() {
     }
 }
 function tulostaTxt15() {
-    if (i === 0) paivitaBin2(4, 3);
     if (i < txt15.length) {
         phase4Ele.innerHTML += txt15.charAt(i);
         i++;
@@ -303,7 +282,6 @@ function tulostaTxt15() {
     }
 }
 function tulostaTxt16() {
-    if (i === 0) paivitaBin1(4, 4);
     if (i < txt16.length) {
         phase4Ele.innerHTML += txt16.charAt(i);
         i++;
@@ -324,7 +302,6 @@ function prosenttiLataus3() {
             latausProsentti = 0;
             i = 0;
             pohjaTeksti = "";
-            paivitaBin2(4, 5)
             phase4Ele.innerHTML += "<br><b>> VAULT_OPENED: PROJECT_ACCESS_GRANTED</b>";
             setTimeout(siirryPhase5, vaiheViive);
         }, statusViive);
@@ -335,7 +312,6 @@ function siirryPhase5() {
     phase4Ele.style.display = "none";
     phase5Ele.style.visibility = "visible";
 
-    if (i === 0) paivitaBin1(5, 1);
     if (i === 0) {
         phase5Ele.innerHTML += "<br><br>";
     }
@@ -351,7 +327,6 @@ function siirryPhase5() {
     }
 }
 function tulostaTxt18() {
-    if (i === 0) paivitaBin2(5, 2);
     if (i < txt18.length) {
         phase5Ele.innerHTML += txt18.charAt(i);
         i++;
@@ -363,7 +338,6 @@ function tulostaTxt18() {
     }
 }
 function tulostaTxt19() {
-    if (i === 0) paivitaBin1(5, 3);
     if (i < txt19.length) {
         phase5Ele.innerHTML += txt19.charAt(i);
         i++;
@@ -384,45 +358,11 @@ function prosenttiLataus4() {
             latausProsentti = 0;
             i = 0;
             pohjaTeksti = "";
-            paivitaBin2(5, 4);
             phase5Ele.innerHTML += "<br><b>> WELCOME_USER. SELECT_PROJECT_TO_BEGIN</b>";
             enterReady();
         }, statusViive);
     }
 }
-
-
-
-// Apufunktiot paivitaBin1 ja paivitaBin2 binäärien päivittymiseen
-function paivitaBin1(vaihe, rivi) {
-    const b1 = document.getElementById("binaariLuvut1");
-    const b2 = document.getElementById("binaariLuvut2");
-
-    b1.textContent = vaihe.toString(2).padStart(8, '0') + "  " + rivi.toString(2).padStart(8, '0');
-    b1.style.left = (Math.floor(Math.random() * 40) + 5) + "vw";
-    b1.style.top = (Math.floor(Math.random() * 20) + 5) + "vh";
-
-    b1.style.opacity = "0.5";
-
-    if (b2) {
-        b2.style.opacity = "0";
-    }
-}
-function paivitaBin2(vaihe, rivi) {
-    const b1 = document.getElementById("binaariLuvut1");
-    const b2 = document.getElementById("binaariLuvut2");
-
-    b2.textContent = vaihe.toString(2).padStart(8, '0') + " " + rivi.toString(2).padStart(8, '0');
-    b2.style.left = (Math.floor(Math.random() * 40) + 5) + "vw";
-    b2.style.top = (Math.floor(Math.random() * 20) + 5) + "vh";
-
-    b2.style.opacity = "0.5";
-
-    if (b1) {
-        b1.style.opacity = "0";
-    }
-}
-
 
 
 // Funktio joka käynnistys sekvenssin jälkeen muuttaa overclock button niin
@@ -447,25 +387,18 @@ function enterReady() {
 }
 
 
-
-// Pääsisältö ja sen funktio joka piilottaa käynnistyksen silmän ja boot-screenin
+// Pääsisältö ja sen funktio joka piilottaa boot-screenin
 // sekä tuo pääsisällön näkyviin
 function avaaMainHud() {
     const bootScreen = document.getElementById("boot-screen");
     const mainContent = document.getElementById("main-content");
-    const cyberEye = document.querySelector(".cyber-eye");
-
-    cyberEye.style.animation = "none";
-    cyberEye.style.opacity = "1";
 
     // Häivytetään latausruutu ja taustakuvat pois
     setTimeout(() => {
         const fadeOut = "opacity 1.5s ease-out";
         bootScreen.style.transition = fadeOut;
-        cyberEye.style.transition = fadeOut;
 
         requestAnimationFrame(() => {
-            cyberEye.style.opacity = "0";
             bootScreen.style.opacity = "0";
         });
     }, 50);
@@ -474,7 +407,6 @@ function avaaMainHud() {
     // Tuodaan varsinainen päänäkymä näkyviin
     setTimeout(() => {
         bootScreen.style.display = "none";
-        cyberEye.style.display = "none";
         
         mainContent.style.display = "block";
 
@@ -488,20 +420,17 @@ function avaaMainHud() {
 }
 
 
-
 // Pääsisällön animaatiot (flickerit)
 function kaynnistaHudElementit() {
     const header = document.getElementById("main-header");
     const footer = document.querySelector(".footer-container");
     const title = document.querySelector(".header-center-title");
     const navbarBrand = document.querySelector(".navbar-brand");
-    const sivusto = document.querySelector(".sivusto");
 
     if (header) {
         header.classList.add("is-active");
         title.classList.add("system-flicker");
         navbarBrand.classList.add("system-flicker");
-        sivusto.classList.add("system-flicker");
     }
 
     updateBraille();
@@ -529,7 +458,6 @@ function updateBraille() {
     }
     setTimeout(updateBraille, Math.random() * 2000 + 500);
 }
-
 
 
 // Projektikorttien data
@@ -565,9 +493,9 @@ window.closeProject = function() {
     if (card) {
         setTimeout(() => {
             card.classList.remove("sliding-out");
-            win.style.display = "none";
         }, 400);
     }
+    setTimeout(() => { win.style.display = "none"; }, 400);
 };
 document.addEventListener("DOMContentLoaded", () => {
     const unit01 = document.getElementById("unit-01");
@@ -605,7 +533,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-
     // Projektiekorttien salauksen purku ja scramble efekti
     function finalizeDecryption(element, actionElement, finalName, card) {
         const chars = "⠁⠂⠃⠄⡀⡁⡂⡃⡄⢀⢁⢂⢃⢄⣀⣁⣂⣃⠃⠗⠁⠊⠇⠵⠴⠷⠦";
@@ -636,7 +563,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
 // Tapahtuma ketju sivuun latauksen yhteydessä
 window.onload = () => {
 
@@ -660,25 +586,12 @@ window.onload = () => {
             overclockBtn.style.borderColor = "#FF0000";
             overclockBtn.style.boxShadow = "0 0 15px #FF0000";
 
-            const binaari1 = document.getElementById("binaariLuvut1");
-            const binaari2 = document.getElementById("binaariLuvut2");
-            binaari1.style.color = "#FF0000";
-            binaari1.style.textShadow = "0 0 5px #FF0000";
-            binaari2.style.color = "#FF0000";
-            binaari2.style.textShadow = "0 0 5px #FF0000";
-
             const phaseElements = [phase1Ele, phase2Ele, phase3Ele, phase4Ele, phase5Ele];
             phaseElements.forEach(el => {
                 el.style.color = "#FF0000";
                 el.style.textShadow = "0 0 5px #FF0000";
                 el.style.borderColor = "#FF0000";
             })
-
-            const eyeEle = document.querySelector(".cyber-eye");
-            if (eyeEle) {
-                eyeEle.style.animation = "eye-pulse 2s linear infinite";
-                eyeEle.style.opacity = "1";
-            }
         };
     };
     
